3e6b7961230d9cdee7c64c4461c1e3d1f02ce2eb
Aristotelis Christou <telischristou@gmail.com>
2013-09-11 19:16:11 +0300

    Refactor javascript to use onle one global variable
    call methods from the namespace RP

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-=A RevPAR calculator
+#A RevPAR calculator
 
 RevPAR is the most usefull KPI(key perfomance indicator) for 
 the practice Revenue Management in the hospitality industry.
 
 Its power comes from the combination of information on occupancy and 
-total available capacity to produce a metric that can be used to compare the effectivenes of revenue management.
+total available capacity to produce a metric that can measure and compare the profitability of a hotel.
 
 
-This web app is built using bootstrap and jquery.
+This web app is built with using bootstrap and jquery.
 
-Author: Aristotelis Christou
-Contact: telischristou /at/ gmail com
-Twitter: @aristotelis_ch
-
+Author: Aristotelis Christou  
+Contact: telischristou /at/ gmail com  
+Twitter: [@aristotelis_ch](https://twitter.com/aristotelis_ch)  
 
 
 
--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-
-// Caclulate  revpar
-var revpar = function(occupancy, adr) {
-  return ((occupancy/100) * adr).toFixed(2);
-};
-
-var  revparUpdate = function(){
-  var occupancy = +$('#occupancy').val();
-  var adr       = +$('#adr').val();
-  $('#revpar').val(revpar(occupancy, adr));
-};
-
-
-$(document).ready(function(){
-  // When a key is pressed check for the occupancy value and adr 
-  $('#occupancy').on('keyup', function(){
+// a global object for the revpar functionality
+var RP = {
+  setup: function(){
+    // When a key is pressed check for the occupancy value and adr 
+    $('#occupancy').on('keyup', RP.occupancyUpdate);
+    $('#adr').on('keyup', RP.adrUpdate);
+    $(RP.slider1);
+    $(RP.slider2);
+    $('#slider1').on('slidechange', RP.revparUpdate);
+    $('#slider2').on('slidechange',RP.revparUpdate);
+  },
+  revpar: function(occupancy, adr) {
+    return ((occupancy/100) * adr).toFixed(2);
+  },
+  occupancyUpdate: function(){
     var occupancy = +$(this).val();
     var adr       = +$('#adr').val();
-    $('#revpar').val(revpar(occupancy, adr));
-  });
-  $('#adr').on('keyup', function(){
+    $('#revpar').val(RP.revpar(occupancy, adr));
+  },
+  adrUpdate: function(){
     var occupancy = +$('#occupancy').val();
     var adr       = +$(this).val();  
-    $('#revpar').val(revpar(occupancy, adr));
-
-  });
-
-  $('#slider1').on('slidechange', revparUpdate);
-
-  $('#slider2').on('slidechange', revparUpdate);
-});
+    $('#revpar').val(RP.revpar(occupancy, adr));
+  },
+  revparUpdate: function(){
+    var occupancy = +$('#occupancy').val();
+    var adr       = +$('#adr').val();
+    $('#revpar').val(RP.revpar(occupancy, adr));
+  },
+  slider1: function() {
+    $( "#slider1" ).slider({
+      orientation: "vertical",
+    range: "min",
+    min: 0,
+    max: 150,
+    value: 60,
+    slide: function( event, ui ) {
+      $( "#occupancy" ).val( ui.value );
+    }
+    });
+    $( "#occupancy" ).val( $( "#slider1" ).slider( "value" ) );
+  },
+  slider2: function() {
+    $( "#slider2" ).slider({
+      orientation: "vertical",
+    range: "min",
+    min: 0,
+    max: 500,
+    value: 60,
+    slide: function( event, ui ) {
+      $( "#adr" ).val( ui.value );
+    }
+    });
+    $( "#adr" ).val( $( "#slider2" ).slider( "value" ) );
+  }
+};
+// Caclulate  revpar
+$(RP.setup);
